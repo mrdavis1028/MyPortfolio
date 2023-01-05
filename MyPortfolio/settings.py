988e5c3a0a56e5a60f6477fd808eae2765c82adf
Julian D <julian.davis1028@gmail.com>
2023-01-04 22:47:23 -0500

    navbar changes, text editing, back button and image optimization(for gallery pages),

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = ['static']
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = ['static']
+#STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
